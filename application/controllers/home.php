1ad717f283c3a9d49db701e5312b5316f3069ecf
Vannary P Fowler <vannary.pov@gmail.com>
2016-03-10 21:05:26 -0500

    Modigying home.php to utilize controller class

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,31 +1,10 @@
 <?php
 
-class Home {
-    protected $_modelFile;
-    protected $_contentFile;
+/* home controller */
+class Home extends Controller {
 
     // index model function when page is accessed
     public function index($params) {
-        $this->_view($this->_model('home', $params));
-    }
-
-    // creates requested model class
-    protected function _model($model, $params) {
-        $modelName = $model;
-        $this->_modelFile = ucfirst($modelName) . 'Model';
-
-        // includes all class files of model
-        foreach ( glob('application/models/' . $this->_modelFile . '/*.php') as $fileName) {
-        include $fileName;
-        }
-
-        // creates an instance of HomeModel();
-        return new $this->_modelFile($model, $params);
-    }
-
-    // creates a view of the creadted model class
-    protected function _view( $data = array() ) {
-        $this->_contentFile = 'application/models/' . $this->_modelFile . '/index.html';
-        include 'public/templates/index.html';
+        $this->_view($this->_model('Home', $params));
     }
 }
