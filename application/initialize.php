5d8454f8f26b33b611df97897a4c40d3d629aa30
Vannary P Fowler <vannary.pov@gmail.com>
2016-03-10 20:55:24 -0500

    Including core controller class in initialize.php

--- application/initialize.php
+++ application/initialize.php
@@ -1,3 +1,11 @@
 <?php
     // base app class
     include 'core/App.php';
+
+    // base controller class
+    include 'core/Controller.php';
+
+// Begin Session
+if ( session_id() == '' || !isset($_SESSION) ) {
+    session_start();
+}
