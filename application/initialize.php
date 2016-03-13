be71cfe0a7c6d23d8a91cefda645e199d461d52c
Vannary P Fowler <vannary.pov@gmail.com>
2016-03-13 04:25:54 -0400

    Including configuration.php in initialize.php

--- application/initialize.php
+++ application/initialize.php
@@ -5,6 +5,9 @@
     // base controller class
     include 'core/Controller.php';
 
+    // general settings
+    include 'config/configuration.php';
+
 // Begin Session
 if ( session_id() == '' || !isset($_SESSION) ) {
     session_start();
